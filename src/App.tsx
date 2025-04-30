20710682e347c808648574a9c52ec2aef88031c3
Preacher-leee <travis.gordon727@gmail.com>
2025-04-30 12:44:27 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,12 @@ import Layout from './components/layout/Layout';
 
 // Pages
 import Home from './pages/Home';
-import Compare from './pages/Compare';
+import Kompare from './pages/Kompare';
 import Facts from './pages/Facts';
 import Newsletter from './pages/Newsletter';
 import About from './pages/About';
+import Success from './pages/checkout/Success';
+import Cancel from './pages/checkout/Cancel';
 import NotFound from './pages/NotFound';
 
 // Context
@@ -43,10 +45,12 @@ function App() {
           <AnimatePresence mode="wait">
             <Routes location={location} key={location.pathname}>
               <Route path="/" element={<Home />} />
-              <Route path="/compare" element={<Compare />} />
+              <Route path="/kompare" element={<Kompare />} />
               <Route path="/facts" element={<Facts />} />
               <Route path="/newsletter" element={<Newsletter />} />
               <Route path="/about" element={<About />} />
+              <Route path="/checkout/success" element={<Success />} />
+              <Route path="/checkout/cancel" element={<Cancel />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
           </AnimatePresence>
