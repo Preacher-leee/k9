325b7cce295adf6a10c92123d1aec629e73370bc
Preacher-leee <travis.gordon727@gmail.com>
2025-05-06 05:03:47 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { FaInstagram, FaTwitter } from "react-icons/fa";
 
 export default defineConfig({
   plugins: [react()],
@@ -8,4 +9,11 @@ export default defineConfig({
       timeout: 5000
     }
   }
+  export default {
+  build: {
+    rollupOptions: {
+      external: ['react-icons/fa6']
+    }
+  }
+}
 });
